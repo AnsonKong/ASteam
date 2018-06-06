11eb468ca3a21444b721b1ea1afe546f6861354d
AnsonKong <kongweian@163.com>
2018-06-06 10:39:54 +0800

    use throttle to lower the frequency of calling scroll event handler

--- src/directive.js
+++ src/directive.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
+import throttle from 'lodash.throttle'
+
 export default function createDirectives () {
   Vue.directive('scroll', {
     inserted: function (el, binding) {
-      let f = function (evt) {
+      let f = throttle(function (evt) {
         if (binding.value(evt, el)) {
           window.removeEventListener('scroll', f)
         }
-      }
+      }, 500)
       window.addEventListener('scroll', f)
     }
   })
